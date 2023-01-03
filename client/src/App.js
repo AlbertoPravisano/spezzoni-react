bd9627df101eeaca489155affaef16381602c2e1
Alberto Pravisano <alberto.camtv@gmail.com>
2023-01-03 20:33:51 +0100

    init excel parsing

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,36 @@
 import React from "react";
+import { Container, Header, Loader, Segment, Dimmer } from "semantic-ui-react";
+import { read, utils } from "xlsx";
 
 const App = () => {
-  return <div>Componente React</div>;
+  const [dataLoaded, setDataLoaded] = React.useState();
+
+  React.useEffect(() => {
+    fetch("./source.xlsx")
+      .then((data) => {
+        console.log(data);
+        const workbook = read(data);
+        setDataLoaded(workbook);
+      })
+      .catch((error) => console.error(error));
+  }, []);
+
+  return (
+    <Container>
+      <br />
+      <br />
+      <Header>Spezzoni</Header>
+      {!dataLoaded && (
+        <Segment basic>
+          <p />
+          <Dimmer active inverted>
+            <Loader state="active">Caricamento in corso...</Loader>
+          </Dimmer>
+        </Segment>
+      )}
+      {dataLoaded && dataLoaded.SheetNames[0]}
+    </Container>
+  );
 };
 
 export default App;
