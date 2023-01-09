f3587e49b2ab1f7774ef193a797316bb6e277c3b
Alberto Pravisano <alberto.pravisano@beantech.it>
2023-01-09 13:11:50 +0100

    fix path abaco

--- client/src/views/Abaco.js
+++ client/src/views/Abaco.js
@@ -3,10 +3,13 @@ import React from "react";
 const SOURCE_XLSX = "source.xlsx";
 
 const Abaco = () => {
+  const url = new URL(window.location.href);
   return (
     <iframe
       title="Abaco"
-      src={`https://view.officeapps.live.com/op/embed.aspx?src=/${SOURCE_XLSX}`}
+      src={`https://view.officeapps.live.com/op/embed.aspx?src=${
+        url.origin + "/" + SOURCE_XLSX
+      }`}
       style={{ height: "30em", width: "inherit" }}
     />
   );
