33b95670ed1881734a356995d1c2c725a8be91a1
Alberto Pravisano <alberto.pravisano@beantech.it>
2023-07-13 11:34:15 +0200

    from user to account route dashboard

--- client/src/routes.js
+++ client/src/routes.js
@@ -1,5 +1,5 @@
 export const BASE_PATH = "/spezzoni-react";
 export const HOME = "/";
-export const DASHBOARD = "/user";
+export const DASHBOARD = "/account";
 export const ABACO = "/abaco";
 export const SIGNUP = "/signup";
